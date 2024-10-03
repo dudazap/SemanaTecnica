faad03ebb3daa486a2e596abd03f17a11a33fd55
Eduarda <125655570+dudazap@users.noreply.github.com>
2024-10-03 12:09:27 -0300

    Add files via upload

---
+++ 3anosemana/script.js
@@ -0,0 +1,8 @@
+function abrirmenu(){
+    let menu=document.getElementById("menu-inicial");
+    if (menu.style.display==="none" ) {
+        menu.style.display="block" 
+    } else {
+        menu.style.display = "none"
+    }
+};
